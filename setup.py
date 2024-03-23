cb1e59fb44abacb884bb33e104731b8a2d8b274a
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2024-03-24 00:21:18 +0530

    completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='tejas',
+    author_email='tejas05in@yahoo.co.in',
+    packages=find_packages(),
+    install_requires=[]
+
+)
